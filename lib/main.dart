33b868ad746af8e34c9ea9a198862b5d305d8edd
fayadchowdhury <fayad.chowdhury@gmail.com>
2023-07-04 23:45:12 +0600

    Add firebase to the project via Flutterfire configure. Needed to grant Internet permissions in the AndroidManifest.xml file. Also needed to comment out RunnerTests code block in Podfile generated for iOS.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,17 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+// Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
